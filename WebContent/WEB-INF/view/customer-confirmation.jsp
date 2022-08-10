9758dab495ef17b083ea966e84595914fce6e605
Suraj Gupta <surajrocks1997@gmail.com>
2022-08-10 21:48:01 +0530

    validating number range using min and max annotation

--- WebContent/WEB-INF/view/customer-confirmation.jsp
+++ WebContent/WEB-INF/view/customer-confirmation.jsp
@@ -6,5 +6,7 @@
 </head>
 <body>
 	The Customer is confirmed: ${customer.firstName} ${customer.lastName}
+	<br>
+	Free Passes are: ${customer.freePasses}
 </body>
 </html>
--- src/com/demo/springDemo/mvc/Customer.java
+++ src/com/demo/springDemo/mvc/Customer.java
@@ -1,15 +1,29 @@
 package com.demo.springDemo.mvc;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 public class Customer {
 
 	private String firstName;
-	
-	@NotNull(message="Is Required")
-	@Size(min=1, message="Is required")
+
+	@NotNull(message = "Is Required")
+	@Size(min = 1, message = "Is required")
 	private String lastName;
+
+	@Min(value = 1, message = "Min 1")
+	@Max(value = 9, message = "Max 10")
+	private int freePasses;
+
+	public int getFreePasses() {
+		return freePasses;
+	}
+
+	public void setFreePasses(int freePasses) {
+		this.freePasses = freePasses;
+	}
 
 	public String getFirstName() {
 		return firstName;
